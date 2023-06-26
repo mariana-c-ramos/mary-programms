5845fc793678d3c2b340c75139b3b78abc3fa57d
Mariana Ramos <mary.programms@gmail.com>
2023-06-26 19:34:54 +0100

    project structure and folders

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="mary programms' portfolio, full of amazing and creative projects!">
+    <meta name="keywords" content="mary programms,portfolio, frontend development, html, css, javascript, php, reactt">
+    <meta name="author" content="Mary Programmss">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>mary programms</title>
+    <link rel="stylesheet" href="./css/reset.css">
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    
+
+
+
+
+
+
+    <script src="./js/jquery.js"></script>
+    <script src="./js/app.js"></script>
+</body>
+</html>
---
+++ project.md
@@ -0,0 +1,6 @@
+1- folder structure
+2- css reset
+3- jquery in code and CDN
+4- make the homepage with simple boxes just to check responsiveness
+
+COMMIT
